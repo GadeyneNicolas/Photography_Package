b2864fadae7707d8095b0198024f8bb14d3cd32d
GADEYNE <ngadeyne@gmail.com>
2019-07-29 16:43:20 +0200

    Modification de la view home. Modification du Css. Ajout d'images d'illustration.

--- src/View/Pages/viewHome.php
+++ src/View/Pages/viewHome.php
@@ -1,15 +1,28 @@
-<section>
-    <h2>Bannière</h2>
+<section id="banner_home">
+    <div id="banner_home_text">
+        <h1>John Doe</h1>
+        <h2>Photographe Professionnel<h2>
+        <h2>Bordeaux</h2>
+    </div>
+    <div id="portrait_home">
+        <img src="Contents/Illustrations/portrait_home.jpg">
+    </div>
+</section>
+
+<section id="services_home">
+    <h1>Mes <span class="title_color">Services</span></h1>
+</section>
+
+<section id="about_home">
+<div id="about_home_text">
+        <h1>A Propos</h1>
+        <p>Description Description Description Description Description Description Description Description 
+            Description Description Description Description Description Description Description Description 
+            Description</p>
+        <a href="#">En savoir plus</a>
+    </div>
 </section>
 
 <section>
-    <h2>Mes services</h2>
-</section>
-
-<section>
-    <h2>A propos</h2>
-</section>
-
-<section>
-    <h2>Mes photos</h2>
+    <h1>Mes <span class="title_color">Photos</span></h1>
 </section>
